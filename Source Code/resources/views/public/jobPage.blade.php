16a410e9e5ab603761c1ff771d9b7c5cec9a668a
mhmdbdour <mhmdobdour@gmail.com>
2021-05-02 18:53:54 +0300

    V 1.0.2.5

--- Source Code/resources/views/public/jobPage.blade.php
+++ Source Code/resources/views/public/jobPage.blade.php
@@ -1,9 +1,9 @@
 @extends('layout.public')
 @section("title")
-    <title>Qorrah | {{$position->name}} page</title>
-    <meta name="description" content="a platform that provides the link between students who are looking for specialized training
+<title>Qorrah | {{$position->name}} page</title>
+<meta name="description" content="a platform that provides the link between students who are looking for specialized training
  with institutions that have training vacancies">
- <meta name="keywords" content="Qorrah, Job, {{$position->name}} , Training">
+<meta name="keywords" content="Qorrah, Job, {{$position->name}} , Training">
 @endsection
 @section('content')
 
@@ -22,12 +22,16 @@
 							<h5>The Employer</h5>
 							<ul>
 								<li><a href={{ route('profile', ['id'=>$position->User_id]) }}><i class="icon-material-outline-business"></i> {{$company->name}}</a></li>
-								<li><i class="icon-material-outline-location-on"></i> {{$company->city}}, {{$company->address}}</li>
+								@if ($position->city)
+								<li><i class="icon-material-outline-location-on"></i> {{$position->city}} {{$position->address ? ',' : null}} {{$position->address}}</li>
+								@else
+								<li><i class="icon-material-outline-location-on"></i> {{$company->city}} {{$company->address ? ',' : null}} {{$company->address}}</li>
+								@endif
 								<li><i class="icon-line-awesome-balance-scale"></i> {{$position->status}}</li>
 							</ul>
 						</div>
 					</div>
-					
+
 				</div>
 			</div>
 		</div>
@@ -39,13 +43,13 @@
 ================================================== -->
 <div class="container">
 	<div class="row">
-		
+
 		<!-- Content -->
 		<div class="col-xl-8 col-lg-8 content-right-offset">
 
 			<div class="single-page-section">
 				<h3 class="margin-bottom-25">Job Description</h3>
-<p>{{$position->about}}</p>
+				<p>{{$position->about}}</p>
 
 			</div>
 
@@ -57,47 +61,46 @@
 
 					@foreach ($similarPositions as $similar)
 
-						<!-- Job Listing -->
-						<a href="{{ route('job', ['id'=>$similar->position_id]) }}" class="job-listing">
+					<!-- Job Listing -->
+					<a href="{{ route('job', ['id'=>$similar->position_id]) }}" class="job-listing">
 
-							<!-- Job Listing Details -->
-							<div class="job-listing-details">
-								<!-- Logo -->
-								<div class="job-listing-company-logo">
-									<img src={{asset("assets/images/profile/$company->image")}} alt="{{$company->name}}">
-								</div>
-
-								<!-- Details -->
-								<div class="job-listing-description">
-									<h4 class="job-listing-company">{{$similar->name}}</h4>
-									<h3 class="job-listing-title">{{$similar->title}}</h3>
-								</div>
+						<!-- Job Listing Details -->
+						<div class="job-listing-details">
+							<!-- Logo -->
+							<div class="job-listing-company-logo">
+								<img src={{asset("assets/images/profile/$company->image")}} alt="{{$company->name}}">
 							</div>
 
-							<!-- Job Listing Footer -->
-							<div class="job-listing-footer">
-								<ul>
-									<li><i class="icon-material-outline-location-on"></i> {{$similar->city}}</li>
-									<li><i class="icon-material-outline-business-center"></i> {{$similar->type}}</li>
-									@if ( ($date = (strtotime(date("Y-m-d"))  - strtotime(explode(" " , $similar->created_at)[0]))) < 60*60*24 )
-									<li><i class="icon-material-outline-access-time"></i> {{'today'}} </li>
-									@else 
+							<!-- Details -->
+							<div class="job-listing-description">
+								<h4 class="job-listing-company">{{$similar->name}}</h4>
+								<h3 class="job-listing-title">{{$similar->title}}</h3>
+							</div>
+						</div>
+
+						<!-- Job Listing Footer -->
+						<div class="job-listing-footer">
+							<ul>
+								<li><i class="icon-material-outline-location-on"></i> {{$similar->city}}</li>
+								<li><i class="icon-material-outline-business-center"></i> {{$similar->type}}</li>
+								@if ( ($date = (strtotime(date("Y-m-d")) - strtotime(explode(" " , $similar->created_at)[0]))) < 60*60*24 ) <li><i class="icon-material-outline-access-time"></i> {{'today'}} </li>
+									@else
 									<li><i class="icon-material-outline-access-time"></i> {{ round( ($date)/(60*60*24), 0, PHP_ROUND_HALF_DOWN) . 'd'}}</li>
 									@endif
 									<li><i class="icon-line-awesome-balance-scale"></i> {{$position->status}}</li>
-								</ul>
-							</div>
-						</a>						
+							</ul>
+						</div>
+					</a>
 					@endforeach
 
 
 
-					</div>
-					<!-- Listings Container / End -->
-
 				</div>
+				<!-- Listings Container / End -->
+
+			</div>
 		</div>
-		
+
 
 		<!-- Sidebar -->
 		<div class="col-xl-4 col-lg-4">
@@ -105,50 +108,50 @@
 
 				@if (isset($errors) && count($errors))
 
-                <ul>
-                    @foreach($errors->all() as $error)
-						
-			<div class="notification error closeable">
-				<p>{{ $error }}</p>
-				<a class="close" href="#"></a>
-			</div>
-                    @endforeach
-                </ul>
+				<ul>
+					@foreach($errors->all() as $error)
 
-        @endif
+					<div class="notification error closeable">
+						<p>{{ $error }}</p>
+						<a class="close" href="#"></a>
+					</div>
+					@endforeach
+				</ul>
+
+				@endif
 
 				@if (Auth::check() && Auth::user()->type == 'user')
 
 				@if ($position->status == 'Closed')
 				<a href="" class="apply-now-button inactiveLink">Closed</a>
-				@else 
+				@else
 				<a href="#small-dialog" class="apply-now-button popup-with-zoom-anim">Apply Now <i class="icon-material-outline-arrow-right-alt"></i></a>
 				@endif
 
-					
-					<div id="small-dialog" class="zoom-anim-dialog mfp-hide dialog-with-tabs">
 
-						<!--Tabs -->
-						<div class="sign-in-form">
-					
-							<ul class="popup-tabs-nav">
-								<li><a href="#tab">Apply Now</a></li>
-							</ul>
-					
-							<div class="popup-tabs-container">
-					
-								<!-- Tab -->
-								<div class="popup-tab-content" id="tab">
-									
-									<!-- Welcome Text -->
-									<div class="welcome-text">
-										<p>For</p>
-										<h3> {{$position->name}}</h3>
-									</div>
-										
-									<!-- Form -->
-									<form method="post" action="{{ route('apply') }}" id="ApplyJob"  enctype="multipart/form-data">
-										@csrf
+				<div id="small-dialog" class="zoom-anim-dialog mfp-hide dialog-with-tabs">
+
+					<!--Tabs -->
+					<div class="sign-in-form">
+
+						<ul class="popup-tabs-nav">
+							<li><a href="#tab">Apply Now</a></li>
+						</ul>
+
+						<div class="popup-tabs-container">
+
+							<!-- Tab -->
+							<div class="popup-tab-content" id="tab">
+
+								<!-- Welcome Text -->
+								<div class="welcome-text">
+									<p>For</p>
+									<h3> {{$position->name}}</h3>
+								</div>
+
+								<!-- Form -->
+								<form method="post" action="{{ route('apply') }}" id="ApplyJob" enctype="multipart/form-data">
+									@csrf
 									{{-- coverLetter + attachments --}}
 									<div class="col-xl-12">
 										<div class="submit-field">
@@ -169,45 +172,45 @@
 												<input type="hidden" name="Position_id" value="{{$position->position_id}}">
 											</div>
 										</div>
-									</div>													
-									</form>
-									
-									<!-- Button -->
-									<button class="button full-width button-sliding-icon ripple-effect" type="submit" form="ApplyJob">Apply <i class="icon-material-outline-arrow-right-alt"></i></button>
-					
-								</div>
-					
+									</div>
+								</form>
+
+								<!-- Button -->
+								<button class="button full-width button-sliding-icon ripple-effect" type="submit" form="ApplyJob">Apply <i class="icon-material-outline-arrow-right-alt"></i></button>
+
 							</div>
+
 						</div>
 					</div>
+				</div>
 				@endif
 
 				@if (Auth::check() && Auth::id() == $position->User_id)
 
 
-					<a href="#small-dialog-2" class="apply-now-button popup-with-zoom-anim">Edit Position<i class="icon-material-outline-arrow-right-alt"></i></a>
-				
-					<div id="small-dialog-2" class="zoom-anim-dialog mfp-hide dialog-with-tabs popupForm">
-	
-						<!--Tabs -->
-						<div class="sign-in-form">
-					
-							<ul class="popup-tabs-nav">
-								<li><a href="#tab">Edit </a></li>
-							</ul>
-					
-							<div class="popup-tabs-container">
-					
-								<!-- Tab -->
-								<div class="popup-tab-content" id="tab">
-										
-									<!-- Form -->
-									<form id="addUser" action="/position/{{$position->position_id}}/update" method="post" enctype="multipart/form-data">
-										{{ csrf_field() }}
-										@method('PATCH')
-				
-										<div class="content with-padding padding-bottom-10">
-											<div class="row">										
+				<a href="#small-dialog-2" class="apply-now-button popup-with-zoom-anim">Edit Position<i class="icon-material-outline-arrow-right-alt"></i></a>
+
+				<div id="small-dialog-2" class="zoom-anim-dialog mfp-hide dialog-with-tabs popupForm">
+
+					<!--Tabs -->
+					<div class="sign-in-form">
+
+						<ul class="popup-tabs-nav">
+							<li><a href="#tab">Edit </a></li>
+						</ul>
+
+						<div class="popup-tabs-container">
+
+							<!-- Tab -->
+							<div class="popup-tab-content" id="tab">
+
+								<!-- Form -->
+								<form id="addUser" action="/position/{{$position->position_id}}/update" method="post" enctype="multipart/form-data">
+									{{ csrf_field() }}
+									@method('PATCH')
+
+									<div class="content with-padding padding-bottom-10">
+										<div class="row">
 											{{-- name --}}
 											<div class="col-xl-4">
 												<div class="submit-field">
@@ -313,13 +316,13 @@
 														<option disabled value='select major'>select multi majors</option>
 														@foreach($majors as $major)
 														@if ( in_array($major->major, $positionMajors) )
-															
+
 														<option selected class="batata" value="{{$major->major}}">{{$major->major}}
 														</option>
 														@else
 														<option class="batata" value="{{$major->major}}">{{$major->major}}
 														</option>
-															
+
 														@endif
 														@endforeach
 													</select>
@@ -332,24 +335,24 @@
 											<div class="col-xl-4">
 												<div class="submit-field">
 													<h5>Status</h5>
-													<select class="selectpicker with-border"  data-live-search="true" name="status" data-size="4" value="{{ $position->status }}"  >
+													<select class="selectpicker with-border" data-live-search="true" name="status" data-size="4" value="{{ $position->status }}">
 														<option disabled value='select company'>select status</option>
 														@if (Auth::user()->type != 'company')
 														<option class="batata" value={{'Hidden'}}>{{'Hidden'}}
 														</option>
-														@else 
+														@else
 														@foreach($statuss as $Pstatus)
 														@if ($position->status == $Pstatus)
-															
+
 														<option selected class="batata" value={{$Pstatus}}>{{$Pstatus}}
 														</option>
 														@else
 														<option class="batata" value={{$Pstatus}}>{{$Pstatus}}
 														</option>
-															
+
 														@endif
 														@endforeach
-														@endif 
+														@endif
 													</select>
 													@if ($errors->has('status'))
 													<div class="text-danger">{{ $errors->first('status') }}</div>
@@ -359,51 +362,51 @@
 											{{-- just a space --}}
 											<div class="col-xl-4">
 												<div class="submit-field">
-										
+
 												</div>
 											</div>
 											{{-- cover Image --}}
 											<div class=" col-xl-4 addImage">
-												<div id="cover" class="image " style="background-image: url({{asset("assets/images/profile/$position->cover")}})" >
-												<div class="dashes"></div>
-												<label style="color: #fff ; text-shadow : 0 0 3px #333">Change</label>
+												<div id="cover" class="image " style="background-image: url({{asset("assets/images/profile/$position->cover")}})">
+													<div class="dashes"></div>
+													<label style="color: #fff ; text-shadow : 0 0 3px #333">Change</label>
 												</div>
 												<p>Cover image</p>
-												<input type="file" id="mediaFileCover" name="cover" class="mediaFile"  />
+												<input type="file" id="mediaFileCover" name="cover" class="mediaFile" />
 											</div>
-										{{-- About + attachments --}}
-										<div class="col-xl-12">
-											<div class="submit-field">
-												<h5>about</h5>
-												<textarea cols="30" rows="5" name="about" class="with-border">{{ $position->about }}</textarea>
-												<div class="uploadButton margin-top-30">
-													<input class="uploadButton-input" type="file" name='attachment' accept="image/*, application/pdf" id="upload" />
-													<label class="uploadButton-button ripple-effect" for="upload">Change
-														attachment</label>
-													<span class="uploadButton-file-name">{{$position->attachment}}</span>
+											{{-- About + attachments --}}
+											<div class="col-xl-12">
+												<div class="submit-field">
+													<h5>about</h5>
+													<textarea cols="30" rows="5" name="about" class="with-border">{{ $position->about }}</textarea>
+													<div class="uploadButton margin-top-30">
+														<input class="uploadButton-input" type="file" name='attachment' accept="image/*, application/pdf" id="upload" />
+														<label class="uploadButton-button ripple-effect" for="upload">Change
+															attachment</label>
+														<span class="uploadButton-file-name">{{$position->attachment}}</span>
+													</div>
 												</div>
 											</div>
-										</div>
-										<input type="hidden" name="user_id" value="{{Auth::user()->user_id}}">
-	
+											<input type="hidden" name="user_id" value="{{Auth::user()->user_id}}">
+
 
 										</div>
 									</div>
-									
+
 									<!-- Button -->
 									<button class="button full-width button-sliding-icon ripple-effect" id="submitButton" type="submit" form="addUser">Apply Changes<i class="icon-material-outline-arrow-right-alt"></i></button>
-					
-								</div>
-					
+
 							</div>
+
 						</div>
 					</div>
+				</div>
 
 
 				@endif
 
 				@if (!(Auth::check()))
-				<a href="{{ route('register') }}" class="apply-now-button " >Register Now <i class="icon-material-outline-arrow-right-alt"></i></a>
+				<a href="{{ route('register') }}" class="apply-now-button ">Register Now <i class="icon-material-outline-arrow-right-alt"></i></a>
 
 				@endif
 
@@ -421,7 +424,7 @@
 									<h5>{{$position->city}}, {{$position->address}}</h5>
 									@else
 									<h5>{{$company->city}}, {{$company->address}}</h5>
-										
+
 									@endif
 								</li>
 								<li>
@@ -429,16 +432,15 @@
 									<span>Job Type</span>
 									<h5>{{$position->type}}</h5>
 								</li>
-								
+
 								<li>
 									<i class="icon-material-outline-access-time"></i>
 									<span>Date Posted</span>
-									@if ( ($date = (strtotime(date("Y-m-d"))  - strtotime(explode(" " , $position->created_at)[0]))) < 60*60*24 )
-									<h5>{{'today'}}</h5>
-									@else 
-									<h5>{{ round( ($date)/(60*60*24), 0, PHP_ROUND_HALF_DOWN) . ' d'}}</h5>
-									
-									@endif									
+									@if ( ($date = (strtotime(date("Y-m-d")) - strtotime(explode(" " , $position->created_at)[0]))) < 60*60*24 ) <h5>{{'today'}}</h5>
+										@else
+										<h5>{{ round( ($date)/(60*60*24), 0, PHP_ROUND_HALF_DOWN) . ' d'}}</h5>
+
+										@endif
 								</li>
 								<li>
 									<i class="icon-feather-award"></i>
@@ -446,18 +448,18 @@
 
 								</li>
 							</ul>
-								@if ($positionSkills)
-								<div class="sidebar-widget">
-									<div class="task-tags">
-										@foreach ($positionSkills as $skill)
-										@if ($skill !='')
-										<span>{{$skill}}</span>
-										@endif
-										@endforeach
+							@if ($positionSkills)
+							<div class="sidebar-widget">
+								<div class="task-tags">
+									@foreach ($positionSkills as $skill)
+									@if ($skill !='')
+									<span>{{$skill}}</span>
+									@endif
+									@endforeach
 
-									</div>
 								</div>
-								@endif
+							</div>
+							@endif
 
 							@if ($positionMajors)
 
@@ -467,14 +469,14 @@
 								<div class="task-tags">
 									@foreach ($positionMajors as $Major)
 									@if ($Major !='')
-										
+
 									<span>{{$Major}}</span>
 									@endif
 									@endforeach
-			
+
 								</div>
 							</div>
-			
+
 							@endif
 
 
@@ -482,17 +484,17 @@
 					</div>
 				</div>
 
-					<!-- Copy URL -->
-					<div class="copy-url">
-						<input id="copy-url" type="text" value="" class="with-border">
-						<button class="copy-url-button ripple-effect" data-clipboard-target="#copy-url" title="Copy to Clipboard" data-tippy-placement="top"><i class="icon-material-outline-file-copy"></i></button>
-					</div>
+				<!-- Copy URL -->
+				<div class="copy-url">
+					<input id="copy-url" type="text" value="" class="with-border">
+					<button class="copy-url-button ripple-effect" data-clipboard-target="#copy-url" title="Copy to Clipboard" data-tippy-placement="top"><i class="icon-material-outline-file-copy"></i></button>
 				</div>
-
 			</div>
-		</div>
 
+		</div>
 	</div>
+
+</div>
 </div>
 
 <script>
@@ -513,13 +515,11 @@
 	});
 
 
-	$('#mediaFileCover').on('change' , function() {
-				let file = $('#mediaFileCover').val();
-			})
-
+	$('#mediaFileCover').on('change', function() {
+		let file = $('#mediaFileCover').val();
+	})
 </script>
 
 
-   
-    @endsection
- 
+
+@endsection
